b2c16a61f5a5b8fb8af54f018991726b38e60e03
phjang0408 <phjang0408@naver.com>
2026-08-09 17:56:06 +0900

    Reflection 2. 수정 및 노션

--- Source/ShooterX/Game/SXGameInstance.cpp
+++ Source/ShooterX/Game/SXGameInstance.cpp
@@ -14,6 +14,7 @@ void USXGameInstance::Init()
 	Super::Init();
 
 	USXUnrealObject* USXObject01 = NewObject<USXUnrealObject>();
+	// 새로운 오브젝트 USXObject01
 	// 언리얼은 이런식으로 new 키워드를 안쓰고 NewObject<>() API를 사용해야 함.
 
 	// 1. 멤버함수 호출방법
@@ -21,10 +22,11 @@ void USXGameInstance::Init()
 	// 우리가 정의한 그냥 멤버함수 호출
 
 	// 2. 리플렉션 시스템을 활용
-	FProperty* NameProperty = USXUnrealObject::StaticClass()->FindPropertyByName(TEXT("Name"));
+	// 범위 지정 연산자로 컴파일 타임 호출 => StaticClass()
+	FProperty* NameProperty = USXUnrealObject::StaticClass()->FindPropertyByName(TEXT("Name"));	// StaticClass() : 클래스 타입 자체를 가져오는 함수,컴파일 타임 <-> GetClass : 런타임
 	FString CompiletimeUSObjectName;
 	if (nullptr != NameProperty) {
-		NameProperty->GetValue_InContainer(USXObject01, &CompiletimeUSObjectName);
+		NameProperty->GetValue_InContainer(USXObject01, &CompiletimeUSObjectName); // 리플렉션 함수
 		UE_LOG(LogTemp, Log, TEXT("CompiletimeUSObjectName: %s"), *CompiletimeUSObjectName);
 	}
 
@@ -32,9 +34,10 @@ void USXGameInstance::Init()
 	USXObject01->HelloUnreal();
 
 	// 2. 리플렉션 시스템을 활용
+	// 포인터 접근 연산자로 런타임에 호출 => GetClass()
 	UFunction* HelloUnrealFunction = USXObject01->GetClass()->FindFunctionByName(TEXT("HelloUnreal"));
 	if (nullptr != HelloUnrealFunction) {
-		USXObject01->ProcessEvent(HelloUnrealFunction, nullptr);
+		USXObject01->ProcessEvent(HelloUnrealFunction, nullptr);	// 리플렉션 함수, nullptr자리는 인자와 반환값이 없을 때 사용하는 방식 : 인자만 있는, 인자+반환값 있는 방식에서 또 다름
 	}
 }
 
