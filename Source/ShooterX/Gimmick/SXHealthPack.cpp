326b7fad6e64dc113b2aa6227ac0da7a60b2c2d6
phjang0408 <phjang0408@naver.com>
2026-08-15 18:13:35 +0900

    Component : 1 생성자 단계

--- Source/ShooterX/Gimmick/SXHealthPack.cpp
+++ Source/ShooterX/Gimmick/SXHealthPack.cpp
@@ -1,4 +1,29 @@
 // SXHealthPack.cpp
 
 #include "Gimmick/SXHealthPack.h"
+#include "Components/SceneComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
+ASXHealthPack::ASXHealthPack() : HealAmount(100.f)
+{
+	// bCanEverTick : 언리얼의 Tick()함수가 매 프레임 호출할 지 결정
+	// true면 수행, flase면 수행하지 않음.
+	PrimaryActorTick.bCanEverTick = false;
+
+
+	// CreateDefaultSubobject<T>(FName name) : 타입 T의 컴포넌트나 서브 오브젝트 생성, 고유 식별자(FName)으로 구분
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SetRootComponent(SceneComponent);
+
+	// [생성자 상황] 에서 사용
+	// CreateDefaultSubobject -> SetupAttachment
+	// 등록은 자동 예약
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(GetRootComponent());
+
+	BodyStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyStaticMeshComponent"));
+	BodyStaticMeshComponent->SetupAttachment(GetRootComponent());
+
+	
+}
--- Source/ShooterX/Gimmick/SXHealthPack.h
+++ Source/ShooterX/Gimmick/SXHealthPack.h
@@ -5,19 +5,34 @@
 #include "GameFramework/Actor.h"
 #include "SXHealthPack.generated.h"
 
+class USceneComponent;
+class UBoxComponent;
+class UStaticMeshComponent;
+
 UCLASS()
 class SHOOTERX_API ASXHealthPack : public AActor
 {
 	GENERATED_BODY()
-	
+
+public:
+	ASXHealthPack();
+
 private:
-	// EditInstanceOnly : 객체에서만 수정 가능(레벨에 배치한 객체에서 Details에서 검색 가능)
-	// BlueprintReadWrite : BP에서 변수를 Get,Set 가능 (ReadOnly는 Get만 가능)
-	// Meta = (AllowPrivateAccess) : private 접근 지정자이지만, 접근을 할 수 있게 해줌.
-	// Category = "ShooterX|SXHealthPack" : 카테고리에 탭을 생성, |로 하면 하위폴더의 개념
 	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Meta = (AllowPrivateAccess),  Category = "ShooterX|SXHealthPack")
 	float HealAmount;
 
 	// bool bIsUsed
 	uint8 bIsUsed : 1;
+
+
+	// 컴포넌트 생성 시, '컴포지션(Has-A)'관계로 생성한다 하였다.
+	// 이를 위해, '값'형식 컴포넌트를, 프로퍼티에 생성한다.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack");
+	TObjectPtr<USceneComponent> SceneComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack");
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (AllowPrivateAccess), Category = "ShooterX|SXHealthPack");
+	TObjectPtr<UStaticMeshComponent> BodyStaticMeshComponent;
 };
