c2e1376094942e124717191cc9ecca3660c73f85
phjang0408 <phjang0408@naver.com>
2026-09-07 22:23:15 +0900

    Control Rotation header

---
+++ Source/ShooterX/Character/SXPlayerCharacter.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/SXPlayerCharacter.h"
+
+ASXPlayerCharacter::ASXPlayerCharacter()
+{
+}
+
+void ASXPlayerCharacter::BeginPlay()
+{
+}
+
+void ASXPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+}
+
+void ASXPlayerCharacter::InputMove(const FInputActionValue& InValue)
+{
+}
